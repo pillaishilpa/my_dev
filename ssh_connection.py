c2c907ef0c792c6cbbc2245457e497399664c6ca
pillaishilpa <41624291+pillaishilpa@users.noreply.github.com>
2018-07-29 11:08:56 +0530

    Update ssh_connection.py

--- ssh_connection.py
+++ ssh_connection.py
@@ -4,7 +4,7 @@ import paramiko
 try:
   ssh=paramiko.SSHClient()
   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  ssh.connect("153.64.30.80",username="root",password="Beagl342"
+  ssh.connect("153.64.30.80",username="root",password="Beagl342")
   print "connection established\n"
   stdin,stdout,stderr=ssh.exec_command("pdestate -a")
   print(stdout.read())
