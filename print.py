12f02b23127f6efcb77dec622affeb53bba2cfb0
pillaishilpa <41624291+pillaishilpa@users.noreply.github.com>
2018-08-13 15:08:37 +0530

    Update print.py

--- print.py
+++ print.py
@@ -2,7 +2,6 @@ import re
 print 'python script trigred from jenlins\n'
 print 'modifying the file to check poll scm functionallity\n'
 print 'adding one more line and few more lines\n'
-print ' adding to check notify commit end point\n'
 print 'finally configured git and jenkins\n'
 print 'cross checking again\n'
 print 'checking jenkins in windowss\n'
