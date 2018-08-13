17f200d2a98212c7e85dedf7d9459461131a1ff8
pillaishilpa <41624291+pillaishilpa@users.noreply.github.com>
2018-08-13 12:21:56 +0530

    Update print.py

--- print.py
+++ print.py
@@ -2,3 +2,4 @@ import re
 print 'python script trigred from jenlins\n'
 print 'modifying the file to check poll scm functionallity\n'
 print 'adding one more line and few more lines\n'
+print ' adding to check notify commit end point\n'
